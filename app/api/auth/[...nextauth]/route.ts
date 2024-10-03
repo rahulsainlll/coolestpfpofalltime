2f50c55400700b20dd31f4e472f80efeab928954
rahulsainlll <rahulsain.social777@gmail.com>
2024-10-03 22:27:21 +0530

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,21 @@
 import NextAuth from "next-auth";
-import XProvider from "next-auth/providers/twitter";
+import TwitterProvider from "next-auth/providers/twitter";
 
 const handler = NextAuth({
   providers: [
-    XProvider({
+    TwitterProvider({
       clientId: process.env.X_CLIENT_ID ?? "",
       clientSecret: process.env.X_CLIENT_SECRET ?? "",
+      version: '1.0a', // Specify OAuth 1.0a
+      authorization: {
+        url: 'https://api.twitter.com/oauth/authenticate',
+        params: { force_login: true }, // Optional: Forces login every time
+      },
     }),
   ],
+  pages: {
+    error: '/auth/error', // Custom error page (optional)
+  },
 });
 
 export { handler as GET, handler as POST };
