c5a1f36100973855d937a9262949bd68c7a9be60
voltycodes <153023205+voltycodes@users.noreply.github.com>
2024-10-11 20:11:51 +0530

    feat(wip:route.js): add user to db on auth

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,12 +1,31 @@
 import Layout from "@/components/layout";
 import ProfileCard from "@/components/ProfileCard";
 
-export default function Leaderboard() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Leaderboard() {
+  const users = await prisma.user.findMany({
+    orderBy: {
+      votes: {
+        _count: 'desc'
+      }
+    },
+    take: 10,
+    include: {
+      votes: true,
+      profilePicture: true
+    }
+  });
+
+  console.log(users);
+  
   return (
     <Layout>
       <h1 className="text-3xl my-6 mb-12 text-center">Leaderboard</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        <ProfileCard />
+        {users && users.map((user, rank) => (
+          <ProfileCard key={user.id} user={user} rank={rank} />
+        ))}
       </div>
     </Layout>
   );
--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,11 +1,13 @@
+import { User } from "@prisma/client";
 import Image from "next/image";
 
-export default function ProfileCard() {
+
+export default function ProfileCard({user, rank}: {user: User, rank: number}) {
   return (
     <div className="flex flex-col items-center justify-center p-4 border-2 rounded-2xl max-w-[200px] mx-auto">
-      <Image src="https://pbs.twimg.com/profile_images/1843616285508415492/qC9Ee_dQ.jpg" className="aspect-square w-full max-w-[200px] h-auto rounded-xl shadow mb-2" alt="volty" width={400} height={400} />
-      <h1 className="text-xl font-bold font-mono">volty</h1>
-      <p className="text-sm">#1</p>
+      <Image src={user.pfpUrl || "/fallbackAvatar.png"} className="aspect-square w-full max-w-[200px] h-auto rounded-xl shadow mb-2" alt={`${user.username}'s pfp`} width={400} height={400} />
+      <h1 className="text-xl font-bold font-mono">{user.username}</h1>
+      <p className="text-sm">#{rank}</p>
     </div>
   )
 }
