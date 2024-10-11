e93196c2b476435010b46e55893d71174d429c7b
Rahul Sain <rahulsain@luffy.local>
2024-10-12 03:52:17 +0530

    try_to_fixed_the_deployment_error

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,30 +1,35 @@
 import Layout from "@/components/layout";
 import ProfileCard from "@/components/ProfileCard";
-
 import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
 
 export default async function Leaderboard() {
-  const users = await prisma.user.findMany({
-    orderBy: {
-      votes: {
-        _count: 'desc'
-      }
-    },
-    take: 10,
-    include: {
-      votes: true,
-      profilePicture: true
-    }
-  });
-  console.log(`user: ${users}`); 
+  let users: User[] = [];
+
+  try {
+    users = await prisma.user.findMany({
+      orderBy: {
+        votes: {
+          _count: 'desc',
+        },
+      },
+      take: 10,
+      include: {
+        votes: true,
+        profilePicture: true,
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+
   console.log('Fetched users:', JSON.stringify(users, null, 2));
 
-  
   return (
     <Layout>
       <h1 className="text-3xl my-6 mb-12 text-center">Leaderboard</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {users && users.map((user, rank) => (
+        {users.map((user, rank) => (
           <ProfileCard key={user.id} user={user} rank={rank + 1} />
         ))}
       </div>
