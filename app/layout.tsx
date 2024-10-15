a3532211e4f73c9030e9461356c0b2f096dfdc9c
rahulsainlll <rahulsain.social777@gmail.com>
2024-10-15 22:31:35 +0530

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 import { KindeSessionProvider } from '@/components/KindeSessionProvider'
 
@@ -33,6 +34,7 @@ export default async function RootLayout({
       >
         <KindeSessionProvider>
         {children}
+        <Analytics />
         </KindeSessionProvider>
       </body>
     </html>
